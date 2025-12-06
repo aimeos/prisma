4460daec2e36d8260c949e2ad31ed92422ca40c9
Aimeos <aimeos@aimeos.org>
2025-12-06 19:19:05 +0100

    Fixed test

--- tests/Providers/Audio/GeminiTest.php
+++ tests/Providers/Audio/GeminiTest.php
@@ -28,7 +28,7 @@ class GeminiTest extends TestCase
             ->describe( AudioFile::fromBinary( 'MP3', 'audio/mpeg' ), 'en' );
 
         $this->assertPrismaRequest( function( $request, $options ) {
-            $this->assertEquals( 'https://generativelanguage.googleapis.com/v1beta/models/gemini-3-pro:generateContent', (string) $request->getUri() );
+            $this->assertEquals( 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent', (string) $request->getUri() );
         } );
 
         $this->assertEquals( 'an audio description', $response->text() );
