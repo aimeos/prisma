a7d63f18e7068e422e29d4881aa3c71b3c608f56
Aimeos <aimeos@aimeos.org>
2025-12-25 14:45:32 +0100

    Implemented revoice() in Murf audio provider

---
+++ src/Providers/Audio/Murf.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Aimeos\Prisma\Providers\Audio;
+
+use Aimeos\Prisma\Contracts\Audio\Revoice;
+use Aimeos\Prisma\Exceptions\PrismaException;
+use Aimeos\Prisma\Files\Audio;
+use Aimeos\Prisma\Providers\Base;
+use Aimeos\Prisma\Responses\FileResponse;
+use Psr\Http\Message\ResponseInterface;
+
+
+class Murf extends Base implements Revoice
+{
+    public function __construct( array $config )
+    {
+        if( !isset( $config['api_key'] ) ) {
+            throw new PrismaException( sprintf( 'No API key' ) );
+        }
+
+        $this->header( 'api-key', $config['api_key'] );
+        $this->baseUrl( $config['url'] ?? 'https://api.murf.ai' );
+    }
+
+
+    public function revoice( Audio $audio, string $voice, array $options = [] ) : FileResponse
+    {
+        $allowed = $this->allowed( $options, ['pitch', 'speed'] );
+
+        $request = $this->request( ['voice_id' => $voice] + $allowed, ['file' => $audio] );
+        $response = $this->client()->post( '/v1/voice-changer/convert', ['multipart' => $request] );
+
+        $this->validate( $response );
+
+        $data = json_decode( $response->getBody()->getContents() );
+
+        return FileResponse::fromUrl( $data?->audio_file );
+    }
+
+
+    protected function validate( ResponseInterface $response ) : void
+    {
+        if( $response->getStatusCode() !== 200 )
+        {
+            $error = json_decode( $response->getBody()->getContents() )?->detail?->message ?: $response->getReasonPhrase();
+            $this->throw( $response->getStatusCode(), $error );
+        }
+    }
+}
