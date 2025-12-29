77b7b39d65c515dab248b4b4e11329c731aec269
Aimeos <aimeos@aimeos.org>
2025-12-29 15:42:10 +0100

    Implemented speak() for Murf audio provider

--- tests/Providers/Audio/MurfTest.php
+++ tests/Providers/Audio/MurfTest.php
@@ -29,4 +29,34 @@ class MurfTest extends TestCase
 
         $this->assertEquals( 'https://murf.ai/link/to/audio/file', $response->url() );
     }
+
+
+    public function testSpeak() : void
+    {
+        $response = $this->prisma( 'audio', 'murf', ['api_key' => 'test'] )
+            ->response( '{
+                "audioFile": "https://murf.ai/link/to/audio/file",
+                "audioLengthInSeconds": 1.1,
+                "remainingCharacterCount": 1,
+                "wordDurations": [{
+                    "endMs": 1,
+                    "startMs": 1,
+                    "word": "string",
+                    "pitchScaleMaximum": 1.1,
+                    "pitchScaleMinimum": 1.1,
+                    "sourceWordIndex": 1
+                }],
+                "encodedAudio": "string",
+                "warning": "string",
+                "consumedCharacterCount": 1
+            }' )
+            ->ensure( 'speak' )
+            ->speak( 'This is a test.', 'test' );
+
+        $this->assertPrismaRequest( function( $request, $options ) {
+            $this->assertEquals( 'https://api.murf.ai/v1/speech/generate', (string) $request->getUri() );
+        } );
+
+        $this->assertEquals( 'https://murf.ai/link/to/audio/file', $response->url() );
+    }
 }
